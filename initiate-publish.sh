fc3b94c56f383802cad159420fc078c1289b4554
tdcmeehan <tim@timdmeehan.com>
2016-01-05 17:17:13 -0500

    Add SNAPSHOTs to build

--- initiate-publish.sh
+++ initiate-publish.sh
@@ -3,22 +3,16 @@
 set -e
 
 if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_REPO_SLUG == "Conductor/rx-ordered-data" && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+    ./gradlew uploadArchives -PnexusUsername="${NEXUS_USERNAME}" -PnexusPassword="${NEXUS_PASSWORD}"
+    RETVAL=$?
 
-    ./gradlew javadoc
+    if [ $RETVAL -eq 0 ]; then
+        echo 'Completed publish!'
+    else
+        echo 'Publish failed.'
+        return 1
+    fi
 
-    # Get to the Travis build directory, configure git and clone the repo
-    cd $HOME
-    git config --global user.email "travis@travis-ci.org"
-    git config --global user.name "travis-ci"
-    git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/Conductor/rx-ordered-data gh-pages > /dev/null
-
-    # Commit and Push the Changes
-    cd gh-pages
-    git rm -rf ./javadoc
-    cp -Rf $TRAVIS_BUILD_DIR/build/docs/javadoc ./javadoc
-    git add -f .
-    git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
-    git push -fq origin gh-pages > /dev/null
 else
     echo "Not on master branch, so not publishing"
     echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
---
+++ publish-javadoc.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_REPO_SLUG == "Conductor/rx-ordered-data" && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+
+    ./gradlew javadoc
+
+    # Get to the Travis build directory, configure git and clone the repo
+    cd $HOME
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "travis-ci"
+    git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/Conductor/rx-ordered-data gh-pages > /dev/null
+
+    # Commit and Push the Changes
+    cd gh-pages
+    git rm -rf ./javadoc
+    cp -Rf $TRAVIS_BUILD_DIR/build/docs/javadoc ./javadoc
+    git add -f .
+    git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+    git push -fq origin gh-pages > /dev/null
+
+else
+    echo "Not on master branch, so not publishing"
+    echo "TRAVIS_BRANCH: $TRAVIS_BRANCH"
+    echo "TRAVIS_REPO_SLUG: $TRAVIS_REPO_SLUG"
+    echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST"
+fi
